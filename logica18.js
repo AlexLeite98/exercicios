613b60a0348ff40bd8c01917c0ac29f865d41f95
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-06 16:43:30 -0300

    update logica18.js missão do bruxo programador

---
+++ logica18.js
@@ -0,0 +1,8 @@
+/**🟠 MISSÃO 3 – A Espada Rúnica (Arrays)
+📜 História:
+Você encontrou uma espada que precisa ser forjada com os 5 maiores números de um conjunto.
+🎯 Objetivo:
+Criar um array com 10 números aleatórios.
+Ordenar em ordem decrescente.
+Mostrar apenas os 5 maiores.
+🏆 Recompensa: +120 XP**/
