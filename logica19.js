5d7ecc347043b839db24fa4d2f78cf84599ba053
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-06 17:34:44 -0300

    update logica19.js

---
+++ logica19.js
@@ -0,0 +1,12 @@
+/**🔴 MISSÃO 4 – O Bruxo Mercador (Sistema)
+📜 História:
+Você virou mercador temporariamente.
+🎯 Objetivo:
+Criar um sistema onde:
+O jogador começa com 100 moedas.
+Pode:
+Comprar espada (-30 moedas)
+Comprar poção (-10 moedas)
+Ver saldo
+Impedir compra se não tiver moedas suficientes.
+🏆 Recompensa: +200 XP**/
