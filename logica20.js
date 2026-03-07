6a43828564756cc3ec6bf0a674f801fe4480d827
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-07 13:45:02 -0300

    update logica20.js

--- logica20.js
+++ logica20.js
@@ -18,11 +18,22 @@ Título desbloqueado: "Bruxo do Código" **/
 let geraldo = 100;
 let boss = 80;
 
-for(i=1; i<=2; i++){
-	alert(`TURNO [${i}]\n\nGeraldo o Bruxo [HP:${geraldo}] X Boss Caçada selvagem [HP:${boss}]`)
+for(i=1; i<=20; i++){
+	alert(`TURNO [${i}]\n\nGeraldo o Bruxo [HP:${geraldo}] X Boss Caçada selvagem [HP:${boss}]`);
 	
 	let atk_geraldo = Math.floor(Math.random()*(20-10 + 1)) + 10;
 	let atk_boss = Math.floor(Math.random()*(15-5 + 1)) + 5;
 	
-	alert(`Geraldo da ${atk_geraldo} de dano!\nBoss da ${atk_boss} de dano!`)
+	alert(`Geraldo da ${atk_geraldo} de dano!\nBoss da ${atk_boss} de dano!`);
+	
+	geraldo -= atk_boss;
+	boss -= atk_geraldo;
+	
+	if(geraldo==0){
+		alert('VOCÊ MORREU!!!');
+		i = 20;
+	}else if(boss==0){
+		alert('Boss foi derrotado, Geraldo venceu!!!');
+		i=20;
+	}
 }
