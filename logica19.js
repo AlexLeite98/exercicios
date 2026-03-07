fe7b253b10fc6ab03aceaebcb7e8237bca5e8dd0
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-07 12:50:54 -0300

    update logica19.js

--- logica19.js
+++ logica19.js
@@ -12,4 +12,17 @@ Impedir compra se não tiver moedas suficientes.
 🏆 Recompensa: +200 XP**/
 //console.log(`${}`); \n
 let moedas = 100
-let loja = Number(prompt(`[SALDO DE $${moedas}]\n O que vc deseja comprar?\n [Espada Élfica - $30]\n [Poção de vida - $10]`));
+
+for(i=0; i<1; i++){
+	let loja = Number(prompt(`[SALDO DE $${moedas}]\n O que vc deseja comprar?\n (1)[Espada Élfica - $30]\n (2)[Poção de vida - $10]\n (3)[Sair da loja]`));
+	
+	if(loja==1){
+		moedas-=30;
+		i-=1;
+	}else if(loja==2){
+		moedas-=10;
+		i-=1;
+	}else if(loja==3){
+		alert('Volte sempre!')
+	}
+}
