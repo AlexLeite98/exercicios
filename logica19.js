2cf377aba9da6e00c54129e474a3d0938115d01a
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-07 12:37:18 -0300

    update logica19.js

--- logica19.js
+++ logica19.js
@@ -10,3 +10,6 @@ Comprar poção (-10 moedas)
 Ver saldo
 Impedir compra se não tiver moedas suficientes.
 🏆 Recompensa: +200 XP**/
+//console.log(`${}`); \n
+let moedas = 100
+let loja = Number(prompt(`[SALDO DE $${moedas}]\n O que vc deseja comprar?\n [Espada Élfica - $30]\n [Poção de vida - $10]`));
