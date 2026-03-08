8f7b88b66716dd5eb40145dcdb9c2a739d047c50
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-08 16:25:24 -0300

    update logica21.js

--- logica21.js
+++ logica21.js
@@ -2,5 +2,5 @@
 //DADOS (d4, d6, d8, d12, d20)
 
 for(i=0; i<2; i++){
-	let dado = Number(prompt('Qual dado vc uer jogar?\n[1] - d4 \n[2] - d6 \n[3] - d8 \n[4] - d12 \n[20] - d20'))
+	let dado = Number(prompt('Qual dado vc uer jogar?\n[1] - d4 \n[2] - d6 \n[3] - d8 \n[4] - d12 \n[5] - d20'))
 }
