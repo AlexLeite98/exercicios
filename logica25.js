a782d1531687f3dc8d906cee90f05987a16da72e
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-08 19:58:00 -0300

    update logica25.js

--- logica25.js
+++ logica25.js
@@ -9,5 +9,8 @@ Listar itens **/
 //console.log(`${}`); \n
 
 let bag = ['Espada de prata', 'Poção'];
-let itens = ['Espada de Aço', 'Bomba', 'Espada de Aço', 'Cela de Cavalo', 'Poção']
+let itens = ['Espada de Aço', 'Bomba', 'Espada de Aço', 'Cela de Cavalo', 'Poção'];
 
+for(i=0; i<1; i++){
+	let opcao = Number(prompt('Vc deseja adicionar ou remover intens ao seu inventario? \n[1] Adicionar \n[2] Remover'))
+}
