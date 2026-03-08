7b58baad77a22522e6c1c9f2b0a7f04a51b2e50d
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-08 17:06:35 -0300

    update logica21.js

--- logica21.js
+++ logica21.js
@@ -1,8 +1,9 @@
 //console.log(`${}`); \n
 //DADOS (d4, d6, d8, d12, d20)
 
-for(i=0; i<3; i++){
-	let dado = Number(prompt('Qual dado vc quer jogar?\n[1] - d4 \n[2] - d6 \n[3] - d8 \n[4] - d12 \n[5] - d20'));
+for(i=0; i<2; i++){
+	i-= 1
+	let dado = Number(prompt('Qual dado vc quer jogar?\n[1] - d4 \n[2] - d6 \n[3] - d8 \n[4] - d12 \n[5] - d20 \n[6] - Finalizar'));
 	
 	if(dado==1){
 		let d4 = Math.floor(Math.random()*(4-1 + 1)+1);
@@ -10,5 +11,5 @@ for(i=0; i<3; i++){
 	}else if(dado==2){
 		let d6 = Math.floor(Math.random()*(6-1 + 1)+1);
 		alert(d6)
-	}
+	}else if()
 }
