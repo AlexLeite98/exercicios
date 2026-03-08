a8cb0e138bf5258d3d086d7341cb9cbb3109c83e
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-08 18:50:08 -0300

    update logica23.js

---
+++ logica23.js
@@ -0,0 +1,12 @@
+/** 🟡 MISSÃO 7 – A Poção das Vogais
+📜 História:
+Um alquimista precisa contar quantas vogais existem em um ingrediente mágico.
+🎯 Objetivo
+Receber uma palavra
+Contar quantas vogais existem nela **/
+//console.log(`${}`); \n
+
+let plavra = prompt('Diga o ingrediente mágico para saber quanta magia te nele.');
+let ingrediente = plavra.toUpperCase();
+
+alert(ingrediente)
