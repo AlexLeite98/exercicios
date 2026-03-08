fa880ee3b00b5fec55cacdcd2302defd3895bad9
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-08 17:45:34 -0300

    update logica22.js

--- logica22.js
+++ logica22.js
@@ -1,9 +1,9 @@
 /** 🟢 MISSÃO 6 – O Cofre do Ferreiro
-📜 História:
-Um ferreiro esqueceu a senha do seu cofre.
-🎯 Objetivo
-Criar um sistema onde:
-A senha correta é "1234"
+📜 História: 
+Um ferreiro esqueceu a senha do seu cofre. 
+🎯 Objetivo 
+Criar um sistema onde: 
+A senha correta é "1234" 
 O usuário tem 3 tentativas
 Se acertar → "Cofre aberto"
 Se errar 3 vezes → "Cofre bloqueado" **/
