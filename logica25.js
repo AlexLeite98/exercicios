85e85d8fae40554c20548ce1931ecf6954007ea1
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-08 21:18:42 -0300

    update logica25.js

--- logica25.js
+++ logica25.js
@@ -15,6 +15,23 @@ for(i=0; i<1; i++){
 	let opcao = Number(prompt('Vc deseja adicionar ou remover intens ao seu inventario? \n[1] Adicionar \n[2] Remover \n[3] Sair'));
 	
 	if(opcao==1){
-		let add = Number(prompt(`O que vc deseja adicionar \n[${itens[0]}] \n[${itens[1]}] \n[${itens[2]}] \n[${itens[3]}] \n[${itens[4]}]`));
+		let add = Number(prompt(`O que vc deseja adicionar \n[1] ${itens[0]} \n[2] ${itens[1]} \n[3] ${itens[2]} \n[4] ${itens[3]} \n[5] ${itens[4]}`));
+		
+		if(add==1){
+			bag.splice(0,0, itens[0]);
+
+		}else if(add==2){
+			bag.splice(0,0, itens[1]);
+			
+		}else if(add==3){
+			bag.splice(0,0, itens[2]);
+			
+		}else if(add==4){
+			bag.splice(0,0, itens[3]);
+			
+		}else if(add==5){
+			bag.splice(0,0, itens[4]);
+			
+		}
 	}
 }
