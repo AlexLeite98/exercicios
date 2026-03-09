24b531a565f9e06e2e485fed83f0c36a475752b5
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-09 00:40:25 -0300

    update logica25.js

--- logica25.js
+++ logica25.js
@@ -20,5 +20,12 @@ let bag = [];
 let itens = ['Espada', 'Poção', 'Bomba'];
 for(i=0; i<1; i++){
 	
-	let opcao = Number(prompt(`o que vc deseja adcionar ao inventario? \n[1] ${itens[0]} \n[2] ${itens[1]} \n[3] ${itens[2]}`))
+	let opcao = Number(prompt(`O que vc deseja adcionar ao inventario? \n[1] ${itens[0]} \n[2] ${itens[1]} \n[3] ${itens[2]}`))
+	if(opcao==1){
+		add(itens[0])
+	}else if(opcao==2){
+		add(itens[1])
+	}else if(opcao==3){
+		add(itens[2])
+	}
 }
